9ad8653fd87a6bc843a676143795187363d7b96c
anadurand <ana.dc1004@gmail.com>
2017-11-17 00:38:56 -0500

    deploy listo

--- src/components/ReactMaps.js
+++ src/components/ReactMaps.js
@@ -5,7 +5,7 @@ import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerW
 
 const MyMapComponent = compose (
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDFz9yo9606yto6BK07KtVS94iTNvsD_ag&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height:`100%`}} />,
         containerElement: <div style= {{ height: `400px`}} />,
         mapElement: <div style={{ height: `100%` }} />,
